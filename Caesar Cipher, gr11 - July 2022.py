af1a38479f1fb59d72313853a3403f94b9373cfb
Pedro Faraco <94463704+itsPedro231@users.noreply.github.com>
2022-11-21 08:26:53 -0500

    Update Caesar Cipher, gr11 - July 2022.py

--- Caesar Cipher, gr11 - July 2022.py
+++ Caesar Cipher, gr11 - July 2022.py
@@ -3,67 +3,48 @@
 # Caesar Cipher It is a type of substitution cipher in which each letter in the 
 # plaintext is replaced by a letter some fixed number of positions down the alphabet.
 
-def encrypt(text, nl):
-  text = text.upper()
-  
-  letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  result = ''
-  valueList = []
-  diff = []
+import os
+def encrypt(text,n):
+  result = ""
+
   for x in range(len(text)):
-    if text[x].isalpha():
-      for y in range(len(letters)):
-        if text[x] == letters[y]:
-          valueList.append(y)
-    else: 
-      diff.append((x,text[x]))
-      valueList.append(999)
-  
-  for x in range(len(valueList)):
-    if valueList[x] == 999:
-      for y in range(len(diff)):
-        for z in range(len(diff[y])):
-          if diff[y][z] == x: result += diff[y][z+1]    
-    else:  
-      result += nl[valueList[x]]
+    char = text[x]
+    n = int(n)
+    
+    if char.isalpha():
+      result += chr((ord(char) + n - 65) % 26 + 65)
+    else: result += char  
+    
   return result
 
-def decrypt(text,nl):
-  text = text.upper()
-  nl = nl.upper()
-  letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  valueList = []
-  result = ''
-  diff = []
+def decrypt(text,n):
+  result = ""
+
   for x in range(len(text)):
-    if text[x].isalpha():
-      for y in range(len(nl)):
-        if text[x] == nl[y]:
-          valueList.append(y)
-    else: 
-      diff.append((x,text[x]))
-      valueList.append(-1)
-  for x in range(len(valueList)):
-    if valueList[x] == -1:
-      for y in range(len(diff)):
-        for z in range(len(diff[y])):
-          if diff[y][z] == x: result += diff[y][z+1]    
-    else:  
-      result += letters[valueList[x]]
-        
+    char = text[x]
+    n = int(n)
+    
+    if char.isalpha():
+      result += chr((ord(char) - n - 65) % 26 + 65)
+    else: result += char  
+    
   return result
+  
+x = str(input('text: '))
+x = x.upper()
+n = str(input('number: '))
+y = input(' 1 - decrypt \n 2 - encrypt \n 3 - encrypt and decrypt \n >> ')
+os.system('clear')
+print("\nPlain Text : " + x)
+print("Shift pattern : " + str(n))
 
-x = input('text: ')
-nl = input('key (26 different letters): ')
-opt = input(' 1 - encrypt \n 2 - decrypt \n 3 - encrypt and decrypt \n >> ')
-if opt == '1':
-  print(encrypt(x, nl))
-elif opt == '2':
-  print(decrypt(x,nl))
-elif opt == '3':
-  x = encrypt(x,nl)
-  print('encrypted message:\n ',x)
-  y = decrypt(x,nl)
-  print('decrypted message:\n',y)
-
-
+if y == '1':
+  print('\ndecrypted message:\n', decrypt(x,n))
+elif y == '2':
+  print('\nencrypted message:\n', encrypt(x,n))
+elif y == '3':
+  x = encrypt(x,n)
+  
+  print('\nencrypted:\n', x)
+  print('\ndecrypted:\n', decrypt(x,n))
+  
