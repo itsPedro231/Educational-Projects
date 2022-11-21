6a8c1cda84bf1b63fb7e6ce6b935ff06cfd3fad1
Pedro Faraco <94463704+itsPedro231@users.noreply.github.com>
2022-11-21 08:13:47 -0500

    Create main.py

---
+++ English detection - gr11, May 2022/main.py
@@ -0,0 +1,30 @@
+# this code gets a text sample and checks if it is written in English
+def readFile():
+  file = open('10000 English Words Frequency Ranked.txt')
+  wordList = []  
+  newLine = ''
+  lines = file.readlines()
+  for line in lines:
+    for word in line:
+      if word.isalpha():
+        newLine += word
+    wordList.append(newLine)
+    newLine = ''
+  file.close()    
+  return wordList  
+
+englishWords= readFile()
+
+text = input('text: ').upper()
+temp = text.split(' ')
+count = 0
+for x in temp:
+    for word in englishWords:
+      word = word.upper()
+      if x == word:
+        count += 1
+        break
+print('\n' + text)   
+print(count, 'recognized words')
+print(str((count/len(temp))*100) + '% English')
+        
